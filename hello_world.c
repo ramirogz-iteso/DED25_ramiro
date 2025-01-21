15e0869cbf096db084c8e32859f4df496caedf99
ramirogz <ramirogz@iteso.mx>
2025-01-20 23:03:11 -0600

    Before class

---
+++ hello_world.c
@@ -0,0 +1,24 @@
+/* Fix this hello world program
+
+  Open the terminal:
+
+  step 1) compile
+    gcc hello_world.c -o hello_world.exe
+
+  does it work? fix it!
+
+  step 2) run it
+    ./hello_world.exe
+
+  TIPS:
+  ls
+
+*/
+
+//#include <stdio.h>
+
+int main(void) 
+{
+  printf("Hello World Welcome to Class #1\n")
+  return 0;
+}
